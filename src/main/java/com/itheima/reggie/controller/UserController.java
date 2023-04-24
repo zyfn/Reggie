70792bb7ae9ac610f156402d559651c091879862
zyfn <947579150@qq.com>
2023-04-24 11:43:44 +0800

    加入redis

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -9,6 +9,7 @@ import com.itheima.reggie.util.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +25,9 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session) throws Exception {
@@ -33,13 +38,17 @@ public class UserController {
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
 
             //调用阿里云短信服务完成发送短信
-            SMSUtils.sendMessage("","",phone,code);
+//            SMSUtils.sendMessage("","",phone,code);
 
             //将验证码保存到session，等待认证
-            session.setAttribute(phone,code);
-            return R.success("验证码发送成功");
+            //session.setAttribute(phone,code);
+
+            //将验证码缓存到redis，并设置有效期一分钟
+            redisTemplate.opsForValue().set(phone,code, 1,TimeUnit.MINUTES);
+
+            return R.success(code);
         }
-        return R.success("验证码发送失败");
+        return R.error("验证码发送失败");
 
     }
 
@@ -47,44 +56,38 @@ public class UserController {
     public R<User> login(@RequestBody Map map, HttpSession session) throws Exception {
         //获取手机号 code
         String phone = map.get("phone").toString();
-//        String code = map.get("code").toString();
-
-        //如果能够比对成功，说明登录成功,因为没有获取验证码，所以直接放行登录
-        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(User::getPhone, phone);
-        User user = userService.getOne(queryWrapper);
-        if (user == null) {
-            //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
-            user = new User() ;
-            user.setPhone(phone);
-            user.setStatus(1);
-            userService.save(user);
-        }
-        session.setAttribute("user",user.getId());
-        return R.success(user);
-
-
-
-        //当真正实现获取验证时 执行下面代码
+        String code = map.get("code").toString();
 
         //从session中取出验证码进行验证
-//        Object codeInSession =session.getAttribute(phone);
+        //Object codeInSession =session.getAttribute(phone);
+
+        //从redis缓存中取出验证码
+        Object codeInSession =redisTemplate.opsForValue().get(phone);
 
         //判断验证码是否正确
-//        if(codeInSession != null && codeInSession. equals(code)) {
-//            //如果能够比对成功，说明登录成功
-//            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
-//            queryWrapper.eq(User::getPhone, phone);
-//            User user = userService.getOne(queryWrapper);
-//            if (user == null) {
-//                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册user = new User() ;
-//                user.setPhone(phone);
-//                user.setStatus(1);
-//                userService.save(user);
-//            }
-//            return R.success(user);
-//        }
-//        return R.error("登录失败");
+        if(codeInSession != null && codeInSession. equals(code)) {
+            //如果能够比对成功，说明登录成功
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null) {
+                //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册user = new User() ;
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user",user.getId());
+
+            //用户登录成功,删除Redis中缓存的验证码
+            redisTemplate.delete(phone);
+
+            return R.success(user);
+        }
+        return R.error("验证码错误或已失效");
+
+
+
+
 
     }
 }
